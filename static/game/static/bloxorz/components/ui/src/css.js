92b21d7ce64077495036802e5e2750282547e2d3
whos-evan <test@gmail.com>
2023-07-20 13:41:57 -0500

    add static content (maybe should be in a different repo?)

---
+++ static/game/static/bloxorz/components/ui/src/css.js
@@ -0,0 +1,14 @@
+(function ($) {
+	$.anwidget('an.CSS', {
+		options: {
+			visible: false
+		},
+		_attrs: ['href'],
+		getAttributes: function () {
+			return this._attrs;
+		},
+		getCreateString: function () {
+			return "<link rel='stylesheet'>";
+		}
+	});
+})(jQuery);
