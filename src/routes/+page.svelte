9b103d55576d48f3a9b0be80d5038fffc852c575
whos-evan <test@gmail.com>
2023-07-27 14:16:37 -0500

    format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,29 @@
 <script lang="ts">
 	import { config } from '$lib/config';
 
-    import Hero from '$lib/components/Hero/Hero.svelte';
-    import Main from '$lib/components/Main/Main.svelte';
+	import Hero from '$lib/components/Hero/Hero.svelte';
+	import Main from '$lib/components/Main/Main.svelte';
 	import Faq from '$lib/components/Faq/Faq.svelte';
 </script>
 
 <svelte:head>
-    <title>{config.branding.name}</title>
+	<title>{config.branding.name}</title>
 	<meta name="description" content="Play and browse for free now on {config.branding.name}!" />
-	<meta property="og:description" content="Play and browse for free now on {config.branding.name}!" />
+	<meta
+		property="og:description"
+		content="Play and browse for free now on {config.branding.name}!"
+	/>
 </svelte:head>
 
 <!-- Sort the elements based on their order in config.layoutOrder -->
 {#each config.homeLayoutOrder as layout}
-    {#if layout === 'hero'}
-        <Hero />
-    {/if}
-    {#if layout === 'faq'}
-        <Faq />
-    {/if}
-    {#if layout === 'main'}
-        <Main />
-    {/if}
+	{#if layout === 'hero'}
+		<Hero />
+	{/if}
+	{#if layout === 'faq'}
+		<Faq />
+	{/if}
+	{#if layout === 'main'}
+		<Main />
+	{/if}
 {/each}
