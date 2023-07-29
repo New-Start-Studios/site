59746072d241640d4a3d7eef6a364b8c6e0c7d68
whos-evan <test@gmail.com>
2023-07-28 20:15:39 -0500

    add admin controls for apps

---
+++ src/routes/admin/apps/+page.ts
@@ -0,0 +1,16 @@
+import type { PageLoad } from './$types';
+import { config } from '$lib/config';
+
+
+export const load = (async ({ fetch }) => {
+	const res = await fetch('/api/admin/user');
+
+	if (res.ok) {
+		return {
+			users: await res.json(),
+            config: config
+		};
+	}
+
+	return {};
+}) satisfies PageLoad;
