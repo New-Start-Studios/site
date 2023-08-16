58aa4ec6a406aab82eed7b5faf3f16d82d757c2e
whos-evan <test@gmail.com>
2023-08-16 12:08:24 -0500

    change that

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -47,9 +47,6 @@
 			.googleFont}:wght@100;200;300;400;500;600;700;800;900&display=swap"
 		rel="stylesheet"
 	/>
-
-	<!-- Favicon -->
-	<link rel="icon" href="/favicon.ico" />
 </svelte:head>
 
 <Gtm gtmId={config.analytics.gtmID} />
