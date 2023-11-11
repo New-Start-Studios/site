2ad0ce4767147b463884cfeaf2f9e9914c7b10ef
whos-evan <test@gmail.com>
2023-11-11 13:50:14 -0600

    remove it as it doesn't work

--- src/routes/api/admin/apps/delete/+server.ts
+++ src/routes/api/admin/apps/delete/+server.ts
@@ -1,6 +1,6 @@
 // DELETE request to create a delete app
 import prisma from '$lib/prisma';
-import type { App } from '@prisma/client';
+import type { App, User} from '@prisma/client';
 
 interface Opts {
 	url: URL;
@@ -22,33 +22,6 @@ export async function DELETE({ url }: Opts): Promise<Response> {
 	} catch (err) {
 		return new Response(err);
 	}
-
-	// Remove every instance that has this game in someone's loved_games or played_games
-	await prisma.user.findMany().then((users) => {
-		for (let i = 0; i < users.length; i++) {
-			const user = users[i];
-			let loved_apps = user.loved_apps;
-			let played_apps = user.played_apps;
-
-			if (loved_apps.includes(id)) {
-				loved_apps.splice(loved_apps.indexOf(id), 1);
-			}
-
-			if (played_apps.includes(id)) {
-				played_apps.splice(played_apps.indexOf(id), 1);
-			}
-
-			prisma.user.update({
-				where: {
-					id: user.id
-				},
-				data: {
-					loved_apps,
-					played_apps
-				}
-			});
-		}
-	});
 
 	return new Response(JSON.stringify(app));
 }
